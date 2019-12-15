92e06cf5ad31eb3bdc789af26c4d41bf19b4ed89
curiousTauseef <18098441+curiousTauseef@users.noreply.github.com>
2019-12-15 22:12:06 +0100

    troubleshoot click.js

--- assets/js/click.js
+++ assets/js/click.js
@@ -1,2 +1,15 @@
+<script type="text/javascript">
+
+var myLink = document.getElementById('clickMe');
+
+myLink.onclick = function(){
+
+	 alert('hello!');
+
+
+}
+
+
 //- Using an anonymous function:
-document.getElementById("clickMe").onclick = doFunction () { alert('hello!'); };
+//document.getElementById("clickMe").onclick = doFunction () { alert('hello!'); };
+</script>
