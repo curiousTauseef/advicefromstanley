c34e207de971ac4da37a0692db1e0c492edf070f
curiousTauseef <18098441+curiousTauseef@users.noreply.github.com>
2019-12-15 20:27:41 +0100

    try js for quote

--- assets/js/advice.js
+++ assets/js/advice.js
@@ -3,3 +3,9 @@ function changeText(text) {
   document.getElementById('chgtext').innerHTML = text;
 
 }
+
+		function parseQuote(response)
+		{
+			document.getElementById("quote").innerHTML = response.quoteText;
+			document.getElementById("author").innerHTML = response.quoteAuthor;
+		}
