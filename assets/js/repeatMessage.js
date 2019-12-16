a6f8cf855d9d94230fe0a94f2aaa31208e250278
curiousTauseef <18098441+curiousTauseef@users.noreply.github.com>
2019-12-16 13:55:27 +0100

    tweak for rpt msg

--- assets/js/repeatMessage.js
+++ assets/js/repeatMessage.js
@@ -2,5 +2,18 @@
         function showMessage(){
             var message = document.getElementById("message").value;
             display_message.innerHTML= message;
+
+
+			//var name = document.forms["RegForm"]["Name"]; 
+
+
+			if (message == "Tauseef")                                  
+    {
+        window.alert("Tauseef, you did the task!");
+        message.focus();
+        return false;
+    }
+
+
         }
       	</script>
