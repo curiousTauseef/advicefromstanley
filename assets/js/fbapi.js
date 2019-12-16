db7bcbd6f5f53f244464bdf070abd2e0beb51739
curiousTauseef <18098441+curiousTauseef@users.noreply.github.com>
2019-12-17 00:26:10 +0100

    make JS respect document readiness

--- assets/js/fbapi.js
+++ assets/js/fbapi.js
@@ -1,12 +1,11 @@
 <script type="text/JavaScript">
-$(document).ready( function()  {
-	function showMessage(){
+window.onload = function showMessage(){
             var message = document.getElementById("message").value;
             display_message.innerHTML= message;
 
-	};
+}
 
 
 
-        });
+
 </script>
