b33ed9271faec312ffab39e6e6686d9dd5d68c78
curiousTauseef <18098441+curiousTauseef@users.noreply.github.com>
2019-12-17 00:07:26 +0100

    start fbapi work

---
+++ assets/js/fbapi.js
@@ -0,0 +1,11 @@
+<script type="text/JavaScript">
+        function showMessage(){
+            var message = document.getElementById("message").value;
+            display_message.innerHTML= message;
+
+
+
+
+
+        }
+</script>
