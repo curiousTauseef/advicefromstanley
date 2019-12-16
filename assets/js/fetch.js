09412d6b45911b66c821dab734bdd55e46cc9eeb
curiousTauseef <18098441+curiousTauseef@users.noreply.github.com>
2019-12-16 19:36:27 +0100

    use js instead of script in html

---
+++ assets/js/fetch.js
@@ -0,0 +1,23 @@
+<script type="text/javascript">
+
+        document.getElementById('fetchUserDataBtn').addEventListener('click', fetchUserData);
+
+        function fetchUserData(){
+            fetch('https://jsonplaceholder.typicode.com/users/')
+                .then(response => response.json())
+                .then(users => {
+                    let output = '<h2>Lists of Users</h2>';
+                    output += '<ul>';
+                    users.forEach(function(user) {
+                        output += `
+                            <li>
+                                ${user.name}
+                            </li>
+                        `;
+                    });
+                    output += '</ul>'
+                    document.getElementById("response").innerHTML = output;
+                });
+        }
+}
+</script>
